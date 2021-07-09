de8e1cbc538ba354ee9117770a9633d4d7c2ccd8
pawel idzikowski <iddziku@gmail.com>
2021-07-09 15:05:45 +0200

    Add db sync to server.js

--- backend/server.js
+++ backend/server.js
@@ -26,3 +26,9 @@ const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
+
+
+// db sync without dropping data
+const db = require("./app/models");
+
+db.sequelize.sync();
