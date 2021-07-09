dcfb60dc168d8376705bf8f8a7f0a1cc5be0fd83
pawel idzikowski <iddziku@gmail.com>
2021-07-10 00:41:58 +0200

    Add middleware for SignUp and verifying token

---
+++ backend/app/middleware/verifySignUp.js
@@ -0,0 +1,24 @@
+const db = require("../models");
+const User = db.user;
+
+checkDuplicateEmail = (req, res, next) => {
+    User.findOne({
+        where: {
+            email: req.body.email
+        }
+    }).then(user => {
+        if (user) {
+            res.status(400).send({
+                message: "Failed! Email is already in use!"
+            });
+            return;
+        }
+        next();
+    });
+};
+  
+const verifySignUp = {
+    checkDuplicateEmail: checkDuplicateEmail
+};
+
+module.exports = verifySignUp;
