fd09d04587388d479d526081b27a81c444a8402a
pawel idzikowski <iddziku@gmail.com>
2021-07-09 18:49:31 +0200

    Update db.config.js

--- backend/app/config/db.config.js
+++ backend/app/config/db.config.js
@@ -1,17 +1,9 @@
-export const HOST = "localhost";
-export const USER = "root";
-export const PASSWORD = "123456";
-export const DB = "testdb";
-export const dialect = "mysql";
-export const pool = {
-    max: 5,
-    min: 0,
-    acquire: 30000,
-    idle: 10000
 module.exports = {
     HOST: "localhost",
     USER: "root",
+    PASSWORD: "YOUR_PASSWORD",
     DB: "tonodedodb",
+    PORT: 0000,
     dialect: "mysql",
     pool: {
       max: 5,
