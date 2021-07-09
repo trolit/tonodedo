a1fa9cffda0b94d489c00fef70a4d09c126562ec
pawel idzikowski <iddziku@gmail.com>
2021-07-09 14:24:50 +0200

    Initialize db config

---
+++ backend/app/config/db.config.js
@@ -0,0 +1,11 @@
+export const HOST = "localhost";
+export const USER = "root";
+export const PASSWORD = "123456";
+export const DB = "testdb";
+export const dialect = "mysql";
+export const pool = {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+};
