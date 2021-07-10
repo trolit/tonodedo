0656f8599e68ba75acaf4b000c081fbdb53cefc1
pawel idzikowski <iddziku@gmail.com>
2021-07-10 15:37:23 +0200

    Add autoIncrement to task model

--- backend/app/models/task.model.js
+++ backend/app/models/task.model.js
@@ -4,7 +4,8 @@ module.exports = (sequelize, Sequelize) => {
 
       id: {
         type: Sequelize.INTEGER,
-        primaryKey: true
+        primaryKey: true,
+        autoIncrement: true,
       },
 
       description: {
