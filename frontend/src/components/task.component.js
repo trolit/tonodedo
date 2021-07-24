ed9e21aea43e4727f324da01e62e61da0cdf6fdc
pawel idzikowski <iddziku@gmail.com>
2021-07-25 01:18:02 +0200

    Perform logout if user has inactive token and opens tasks tab

--- frontend/src/components/task.component.js
+++ frontend/src/components/task.component.js
@@ -31,6 +31,12 @@ export default class Task extends Component {
             tasks: response.data,
           });
         }
+      ).catch(err => {
+          if (err.message.includes("401")) {
+            AuthService.logout(); 
+            window.location.reload();
+          }
+        }
       );
     }
   }
